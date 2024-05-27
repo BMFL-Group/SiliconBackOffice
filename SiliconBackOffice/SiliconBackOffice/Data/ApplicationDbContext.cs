b284db639a7fee029117be5e1e1a6da7201fa7c9
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-27 12:46:51 +0200

    Modified

--- SiliconBackOffice/SiliconBackOffice/Data/ApplicationDbContext.cs
+++ SiliconBackOffice/SiliconBackOffice/Data/ApplicationDbContext.cs
@@ -5,5 +5,17 @@ namespace SiliconBackOffice.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<AddressModel> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AddressModel>()
+                .HasMany(a => a.Users)
+                .WithOne(u => u.Address)
+                .HasForeignKey(u => u.AddressId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
     }
 }
