62d6fbce4973db08228d1b0c9028ec6e8b6583fa
Martin <martinkristensen12@gmail.com>
2024-05-27 22:20:14 +0200

    trying to bind courseTitle to savedcourses

--- SiliconBackOffice/SiliconBackOffice/Models/SavedCourses/SavedCoursesModel.cs
+++ SiliconBackOffice/SiliconBackOffice/Models/SavedCourses/SavedCoursesModel.cs
@@ -1,9 +1,13 @@
-﻿namespace SiliconBackOffice.Models.SavedCourses;
+﻿using SiliconBackOffice.Models.Courses;
+
+namespace SiliconBackOffice.Models.SavedCourses;
 
 public class SavedCoursesModel
 {
     public string CourseId { get; set; } = null!;
+    public string Title { get; set; } = null!;
     public string UserId { get; set; } = null!;
     public bool IsBookmarked { get; set; }
     public bool HasJoined { get; set; }
+    public List<Course>? Course { get; set; } 
 }
